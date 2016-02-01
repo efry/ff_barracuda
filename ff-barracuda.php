6251e877e065e9410f14ff34d18eb8b24b300065
ethan.fry <ethan@fusionfarm.com>
2016-02-01 16:04:03 -0600

    new version num

--- ff-barracuda.php
+++ ff-barracuda.php
@@ -7,7 +7,7 @@ Author:      Fusionfarm
 Author URI:  http://fusionfarm.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Version: 1.0.7
+Version: 1.0.8
 Domain Path: /languages
 Text Domain: Barracuda
 */
